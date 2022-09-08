8991966044b7636db2ae48fba12d2ad348636fc3
Edward Cabello <65357604+eddshine@users.noreply.github.com>
2022-09-08 13:51:19 +0800

    One line calculator using Python

---
+++ One_line_calculator.py
@@ -0,0 +1,21 @@
+# RULES
+# 1.No using loop.
+# 2.No using conditional statements.
+
+# Take all inputs from the users.
+# Take 9 random arithmetic symbols.
+randOperators = list(input('Enter 9 random arithmetic op: '))
+
+#Take 10 random numbers separated by semi-colon(;).
+randNumbers = input('Enter 10 random values separated by ";": ').split(';')
+
+# Create an empty list with a size of s=s*(list1 + list2)
+result = [None]*(len(list(randNumbers))+len(randOperators))
+
+# Append list_1 and list_2 in alternating order.
+result[::2] = list(randNumbers)
+result[1::2] = randOperators
+
+# Do all calculations using eval and printing the result after that.
+print(f"The result of {''.join(result)} is {eval(''.join(result))}")
+
